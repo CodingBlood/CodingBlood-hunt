bea147ac849ad447e19478f68b13d784eb712d8c
CodingBlood <kartik.agarwal.192.168@gmail.com>
2022-03-18 12:41:45 +0530

    Hints

--- js/main.js
+++ js/main.js
@@ -9,6 +9,23 @@ function success(pos) {
   document.querySelector("H1").textContent=`Latitude : ${crd.latitude}`;
   document.querySelector("H2").textContent=`Longitude: ${crd.longitude}`;
   document.querySelector("H4").textContent=`More or less ${crd.accuracy} meters.`;
+  if(crd.latitude>=27.215312 && crd.latitude<=27.215452){
+    if(crd.latitude>=27.214898 && crd.latitude<=27.215819){
+      if(crd.longitude>=78.015170 && crd.longitude<=78.016282){
+        if(crd.longitude>=78.015561 && crd.longitude<=78.015711){
+            document.querySelector("p").textContent=`U Made it. Your Clue is : Blah Blah Blah`;
+        }else{
+          document.querySelector("p").textContent=`Not Yet Keep Going`;
+        }
+      }else{
+        document.querySelector("p").textContent=`Not Yet Keep Going`;
+      }
+    }else{
+      document.querySelector("p").textContent=`Not Yet Keep Going`;
+    }
+  }else{
+    document.querySelector("p").textContent=`Not Yet Keep Going`;
+  }
   console.log('Your current position is:');
   console.log(`Latitude : ${crd.latitude}`);
   console.log(`Longitude: ${crd.longitude}`);
